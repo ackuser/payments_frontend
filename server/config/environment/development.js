b507e17e0c001404a06b0339997c1d95c9cdd766
ackuser <mail@ackarim.com>
2015-07-18 17:44:33 +0100

    first commit with structure create by yo angular full stack generator…Coffee, Jade, ng-route, Sass, Bootstrap + UI, Mongosee + MongoDB, Google + Facebook + Twitter Login

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/paymentsfrontend-dev'
+  },
+
+  seedDB: true
+};
